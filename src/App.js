e22f7684db272fb01025370e9f6c0eb18d696a9f
Akila-Sandakelum <sandakelum.akila@gmail.com>
2018-10-04 17:27:57 +0530

    Basic API calls establsihed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: "d9a488ece7b73b809ceb4c3555372d27",
+  secret: "c43753cd5a2dddda900dd88dc34bcb45"
+});
 // Class components
 class App extends React.Component {
-  handleOnClick() {
-    alert("You clicked the title");
+  // Required constructor with props
+  // Super keyword is a must to pass up the props
+  constructor(props) {
+    super(props);
+
+    // Initialize the state in the constructor
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    // Making API call and retrieve the data back and populare pets array
+    petfinder.pet
+      .find({ output: "full", location: "Seatle, WA " })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        //  Shallowcopping
+        this.setState({
+          pets
+        });
+      });
   }
 
   render() {
@@ -29,10 +65,32 @@ class App extends React.Component {
     ]); */
     return (
       <div>
-        <h1 onClick={this.handleOnClick}>Adopt Me!</h1>
-        <Pet name="Luna" animal="Dog" breed="Havaneese" />
-        <Pet name="Pepper" animal="Bird" breed="Cockteil" />
-        <Pet name="Meuw Meuw" animal="Cat" breed="Persion" />
+        <h1>Adopt Me!</h1>
+        {/* <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre> */}
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/Pet.js
+++ src/Pet.js
@@ -1,21 +1,48 @@
 import React from "react";
 
-const Pet = props => {
+/* const Pet = props => {
   /* return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed)
   ]); */
 
-  // We can convert above code like below using JSX
-  // It provides the feeling like we writing html directly
-  return (
+// We can convert above code like below using JSX
+// It provides the feeling like we writing html directly
+/* return (
     <div>
       <h1>{props.name}</h1>
       <h2>{props.animal}</h2>
       <h2>{props.breed}</h2>
     </div>
   );
-};
+}; */
+
+class Pet extends React.Component {
+  // Class should defenitely consists with render() method
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt="{name}" />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal}-{breed}-{location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
