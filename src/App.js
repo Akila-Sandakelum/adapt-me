513886d242e59a95de2e4d227cb8f5a4aafd9ca4
Akila-Sandakelum <sandakelum.akila@gmail.com>
2018-10-10 11:44:53 +0530

    Added code spliting in App js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
+import Loadable from "react-loadable";
 import pf from "petfinder-client";
 import { Provider } from "./SearchContext";
-import Results from "./Results";
 import NavBar from "./NavBar";
-import Loadable from "react-loadable";
-import SearchParams from "./SearchParams";
 
 const petfinder = pf({
   key: "d9a488ece7b73b809ceb4c3555372d27",
@@ -15,6 +13,20 @@ const petfinder = pf({
 
 const LoadableDetails = Loadable({
   loader: () => import("./Details"),
+  loading() {
+    return <h1> loading split up code...</h1>;
+  }
+});
+
+const LoadableResults = Loadable({
+  loader: () => import("./Results"),
+  loading() {
+    return <h1> loading split up code...</h1>;
+  }
+});
+
+const LoadableSearchParams = Loadable({
+  loader: () => import("./SearchParams"),
   loading() {
     return <h1> loading split up code...</h1>;
   }
@@ -82,9 +94,9 @@ class App extends React.Component {
         <NavBar />
         <Provider value={this.state}>
           <Router>
-            <Results path="/" />
+            <LoadableResults path="/" />
             <LoadableDetails path="/details/:id" />
-            <SearchParams path="/search-params" />
+            <LoadableSearchParams path="/search-params" />
           </Router>
         </Provider>
       </div>
